4f66f3a8578395cbcdb736b9e41f24768a3cefa6
Andrey Romanov <andrey.a.romanov@gmail.com>
2021-12-08 20:49:57 +0300

    Add sticky footer

--- index.php
+++ index.php
@@ -86,7 +86,7 @@
     }
 ?>
 <!DOCTYPE html>
-<html lang="ru">
+<html lang="ru" class="h-100">
   <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -103,7 +103,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" integrity="sha256-uxjsS9cYFLTjmlr8j5i+DqvOLCBugRzEeWxUMWZeYXQ=" crossorigin="anonymous">
   </head>
-  <body>
+  <body class="d-flex flex-column h-100">
 
     <nav class="navbar navbar-light mb-5 bg-light">
       <div class="container-fluid">
@@ -176,6 +176,12 @@
         </div>
       </div>
     </div>
+
+    <footer class="footer mt-auto py-3 bg-light">
+      <div class="container">
+        <p class="text-center text-muted mb-0">© 2021 AndRom</p>
+      </div>
+    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
